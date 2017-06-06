7a5ff8241abc2027625c8bf4813a0c88017f85b2
刘达 <imliuda@163.com>
2017-06-06 16:58:04 +0800

    update

--- avl_tree.c
+++ avl_tree.c
@@ -26,26 +26,32 @@ int avl_tree_height(avl_tree **tree) {
     return (*tree)->height;
 }
 
-static void right_rotate(avl_tree *tree) {
-    avl_tree *child = tree->left;
-    tree->left = child->right;
-    child->right = tree;
+static void right_rotate(avl_tree **tree) {
+    printf("right rotate\n");
+    avl_tree *child = (*tree)->left;
+    (*tree)->left = child->right;
+    child->right = *tree;
 
-    tree->height = max(avl_tree_height(&tree->left),
-                       avl_tree_height(&tree->right)) + 1;
+    (*tree)->height = max(avl_tree_height(&(*tree)->left),
+                       avl_tree_height(&(*tree)->right)) + 1;
     child->height = max(avl_tree_height(&child->left),
                        avl_tree_height(&child->right)) + 1;
+
+    *tree = child;
 }
 
-static void left_rotate(avl_tree *tree) {
-    avl_tree *child = tree->right;
-    tree->right = child->left;
-    child->left = tree;
+static void left_rotate(avl_tree **tree) {
+    printf("left rotate\n");
+    avl_tree *child = (*tree)->right;
+    (*tree)->right = child->left;
+    child->left = *tree;
 
-    tree->height = max(avl_tree_height(&tree->left),
-                       avl_tree_height(&tree->right)) + 1;
+    (*tree)->height = max(avl_tree_height(&(*tree)->left),
+                       avl_tree_height(&(*tree)->right)) + 1;
     child->height = max(avl_tree_height(&child->left),
                        avl_tree_height(&child->right)) + 1;
+
+    *tree = child;
 }
 
 void avl_tree_insert(avl_tree **tree, int key, void *data) {
@@ -53,7 +59,6 @@ void avl_tree_insert(avl_tree **tree, int key, void *data) {
         *tree = malloc(sizeof(avl_tree));
         (*tree)->key = key;
         (*tree)->data = data;
-        (*tree)->height = 1;
         (*tree)->left = NULL;
         (*tree)->right = NULL;
     } else if (key <= (*tree)->key) {
@@ -61,21 +66,21 @@ void avl_tree_insert(avl_tree **tree, int key, void *data) {
         if (avl_tree_height(&(*tree)->left) -
             avl_tree_height(&(*tree)->right) == 2) {
             if (key <= (*tree)->left->key) {
-                right_rotate(*tree);
+                right_rotate(tree);
             } else {
-                left_rotate((*tree)->left);
-                right_rotate(*tree);
+                left_rotate(&(*tree)->left);
+                right_rotate(tree);
             }
         }
     } else {
         avl_tree_insert(&(*tree)->right, key, data);
          if (avl_tree_height(&(*tree)->right) -
             avl_tree_height(&(*tree)->left) == 2) {
-            if (key > (*tree)->left->key) {
-                left_rotate(*tree);
+            if (key > (*tree)->right->key) {
+                left_rotate(tree);
             } else {
-                right_rotate((*tree)->right);
-                left_rotate(*tree);
+                right_rotate(&(*tree)->right);
+                left_rotate(tree);
             }
         }
     }
@@ -83,6 +88,7 @@ void avl_tree_insert(avl_tree **tree, int key, void *data) {
                           avl_tree_height(&(*tree)->right)) + 1;
 }
 
+#if 0
 /*
  * ┌ \u250c ┐ \u2510  ┴ \u2534  ─ \u2500
  */
@@ -106,15 +112,13 @@ static void _avl_tree_print_symbol(int padding, int width) {
 static void _avl_tree_print_fill(avl_tree **tree, char *buf[]) {
     
 }
+#endif
 
 void avl_tree_print(avl_tree **tree) {
-    int height = avl_tree_height(tree);
-    char *buf[height];
-    avl_tree *node = *tree;
-    _avl_tree_print_symbol(10, 5);
-    int i;
-    for (i = 0; i < height; i++) {
-        printf("");
+    if (*tree != NULL) {
+        printf("key: %d, height: %d, left: %d, right: %d\n", (*tree)->key, (*tree)->height, (*tree)->left ? (*tree)->left->key : 0, (*tree)->right ? (*tree)->right->key : 0);
+        avl_tree_print(&(*tree)->left);
+        avl_tree_print(&(*tree)->right);
     }
 }
 
@@ -123,11 +127,19 @@ int main() {
 
     srandom(time(NULL));    
     int i;
-    for(i=0; i<10; i++) {
-        avl_tree_insert(&tree, random() % 100, NULL);
-    }
+    //for(i=0; i<7; i++) {
+      //  int num = random() % 100;
+       // printf("%d\n", num);
+        avl_tree_insert(&tree, 52, NULL);
+        avl_tree_insert(&tree, 72, NULL);
+        avl_tree_insert(&tree, 93, NULL);
+        avl_tree_insert(&tree, 11, NULL);
+        avl_tree_insert(&tree, 12, NULL);
+        avl_tree_insert(&tree, 80, NULL);
+        avl_tree_insert(&tree, 19, NULL);
+    //}
 
     printf("tree height: %d\n", avl_tree_height(&tree));
-    //avl_tree_print(&tree);
+    avl_tree_print(&tree);
     return 0;
 }
